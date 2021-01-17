c09668fabc6255c470a31e054f4df26fb9dc9e01
Per Dahlstedt <dahlstedt.per@gmail.com>
2021-01-17 16:07:38 +0100

    Add font and page content

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,64 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <main>
+    <div class="content">
+      <div class="header">
+        <h1 class="title">HAKKERBOJZ</h1>
+      </div>
+      <div class="links">
+        <div v-for="link in links" :key="link.url">
+          <a class="link" :href="link.url">{{ link.display }}</a>
+        </div>
+      </div>
+    </div>
+  </main>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import { Vue, Component } from "vue-property-decorator";
+import * as settings from "../settings.json";
 
-export default {
-  name: "App",
-  components: {
-    HelloWorld
-  }
-};
+@Component()
+class App extends Vue {
+  links = settings.links;
+}
+
+export default App;
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@font-face {
+  font-family: "Clip";
+  src: local("Clip"), url("./fonts/Clip.ttf") format("truetype");
+}
+* {
+  box-sizing: border-box;
+  padding: 0;
+  margin: 0;
+}
+.content {
+  min-height: 100vh;
+  width: 100%;
+  background-color: black;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+.header {
+  padding: 20px;
+}
+.title {
+  font-family: "Clip";
+  color: hotpink;
+  font-size: 100px;
+  font-weight: normal;
+}
+.link {
+  text-decoration: none;
+  font-size: 18px;
+  color: hotpink;
+}
+.link:hover {
+  text-shadow: rgb(255, 159, 207) 0 0 2px;
 }
 </style>
